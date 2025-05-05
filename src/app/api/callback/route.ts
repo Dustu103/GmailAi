532686f4d27bf5d8221390b383a0bf540621fb08
Arnab <arnabpramanik102018@gmail.com>
2025-05-06 03:53:52 +0530

    maybe final

--- src/app/api/callback/route.ts
+++ src/app/api/callback/route.ts
@@ -1,5 +1,5 @@
 // app/api/auth/callback/route.ts
-import { getAccessToken, listGmailLabels } from '../gmail';
+import { getAccessToken, listGmailLabels,watchGmailInbox } from '../gmail';
 
 export async function GET(req: Request) {
   const url = new URL(req.url);
@@ -11,8 +11,9 @@ export async function GET(req: Request) {
 
   try {
     const auth = await getAccessToken(code);
-    const labels = await listGmailLabels(auth);
-    return Response.json({ labels });
+    const watchResponse = await watchGmailInbox(auth);
+    // const labels = await listGmailLabels(auth);
+    return Response.json({ watchResponse });
   } catch (err: unknown) {
     const message = err instanceof Error ? err.message : 'Unknown error';
     return Response.json({ error: message }, { status: 500 });
